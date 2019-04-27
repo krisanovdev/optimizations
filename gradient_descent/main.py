b93d6ddc478faa568939785143fb31ac716d87f2
unknown <antonxxx5@gmail.com>
2019-04-27 22:40:08 +0300

    Refactoring.

--- gradient_descent/main.py
+++ gradient_descent/main.py
@@ -34,30 +34,40 @@ def find_argmin_h_expr(h_expr):
     return solution
 
 
-def check_stop_condition(gradient, epsilon):
+def calculate_norm(vector):
     norm = 0
-    for elem in gradient:
+    for elem in vector:
         norm += elem * elem
-    return sqrt(norm) <= epsilon
+    return sqrt(norm)
 
 
 def main():
-    symbols = init_symbols_list(2)
-    input_func = input('Func: ')
-    func = parse_expr(input_func)
-    current_x = [0, 0]
+    dimension = int(input('Input dimension (num of variables): '))
+    current_x = [int(x) for x in input('Input init guess (x0, splitted by spaces): ').split()]
 
+    if len(current_x) != dimension:
+        raise RuntimeError('Length of x0 not equal to dimension!')
+
+    symbols = init_symbols_list(dimension)
+    func = parse_expr(input('Input function: '))
+    epsilon = float(input('Input accuracy (epsilon): '))
+
+    iteration = 0
     while 1:
-        iter = iter + 1
         gradient = get_gradient(func, symbols, current_x)
-        if check_stop_condition(gradient, 0.001):
+        norm = calculate_norm(gradient)
+        if norm <= epsilon:
             break
+
         h_expr = create_h_expr(func, symbols, current_x, gradient)
         h = find_argmin_h_expr(h_expr)
 
         for i in range(len(current_x)):
             current_x[i] = current_x[i] - h * gradient[i]
-        print(current_x)
+        print('Iteration №{}: x = {}'.format(iteration, current_x))
+        iteration += 1
+
+    print('Gradient norm: {}'.format(norm))
 
 
 if __name__ == '__main__':
